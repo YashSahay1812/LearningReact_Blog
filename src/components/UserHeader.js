38f38ad313acfd348cc777a13b8cf1b6e235a5e6
unknown <yashsahay999@gmail.com>
2022-08-31 11:52:43 +0530

    Passing only required state information to UserHeader component through mapStateToProps

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -4,39 +4,19 @@ import { fetchUser } from "../actions";
 
 const UserHeader = (props) => {
 
-    const renderedUser = props.users.find(user => props.userId === user.id);
-
     useEffect(() => { 
         props.fetchUser(props.userId);
     },[]);
 
     return (
-        <div className="header">{!renderedUser ? "Loading..." : renderedUser.name}</div>
+        <div className="header">{!props.user ? "Loading..." : props.user.name}</div>
     );
 
 }
 
-// class UserHeader extends React.Component {
-//     componentDidMount() {
-//         this.props.fetchUser(this.props.userId);
-//     }
-
-//     render() {
-//         const user = this.props.users.find(user => user.id === this.props.userId);
-
-//         if(!user){
-//             return <div className="header">Loading...</div>;
-//         }
-
-//         return (
-//             <div className="header">{user.name}</div>
-//         );
-//     }
-// }
-
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
     console.log(state.users);
-    return {users: state.users};
+    return {user: state.users.find(user => user.id === ownProps.userId)};
 }
 
 export default connect(mapStateToProps,{fetchUser})(UserHeader);
